08ee712cba5654e906b43cf1ff6ab45eb2233f17
Theo Markovic <theodore.markovic@svenskaspel.se>
2018-03-16 14:26:15 +0100

    fixed paths

--- api/index.js
+++ api/index.js
@@ -4,20 +4,19 @@ const passport = require('passport');
 const config = require('./config');
 
 // connect to the database and load models
-require('./server/models').connect(config.dbUri);
+require('./models').connect(config.dbUri);
 
 const app = express();
 // tell the app to look for static files in these directories
-app.use(express.static('./server/static/'));
-app.use(express.static('./client/dist/'));
+app.use(express.static('./static/'));
 // tell the app to parse HTTP body messages
 app.use(bodyParser.json());
 // pass the passport middleware
 app.use(passport.initialize());
 
 // load passport strategies
-const localSignupStrategy = require('./server/passport/local-signup');
-const localLoginStrategy = require('./server/passport/local-login');
+const localSignupStrategy = require('./passport/local-signup');
+const localLoginStrategy = require('./passport/local-login');
 passport.use('local-signup', localSignupStrategy);
 passport.use('local-login', localLoginStrategy);
 
@@ -25,7 +24,7 @@ passport.use('local-login', localLoginStrategy);
 
 
 // routes
-const apiRoutes = require('./server/routes/api');
+const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
 
